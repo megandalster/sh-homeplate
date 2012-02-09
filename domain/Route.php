52d26ed3dd7a0eb08a1cf0a1f4e7a384ba892be3
Richardo Hopkins <milkywayw@gmail.com>
2012-02-08 23:16:17 -0500

    Made constructor for the Route class and created getter functions for all the fields. Also completed the testRoute.php to allow testing

--- domain/Route.php
+++ domain/Route.php
@@ -8,5 +8,37 @@ class Route {
 	private $stops;			// array of stops for this Route
     private $day;	      	// string name of this day "Monday"...
     private $notes;			// notes written by the team captain 
+
+		/**
+         * constructor for a Route
+         */
+    function __construct($id, $drivers, $teamcaptain_id, $stops, $day, $notes){
+    	$this->id = $id;
+    	$this->drivers = explode(',', $drivers);
+    	$this->teamcaptain_id = $teamcaptain_id;
+    	$this->stops = explode(',', $stops);
+    	$this->day = $day;
+    	$this->notes = $notes;
+    }
+    
+    // getter functions
+    function get_id() {
+    	return $this->id;
+    }
+    function get_drivers() {
+    	return $this->drivers;
+    }
+    function get_teamcaptain_id() {
+    	return $this->teamcaptain_id;
+    }
+    function get_stops() {
+    	return $this->stops;
+    }
+    function get_day() {
+    	return $this->day;
+    }
+    function get_notes() {
+    	return $this->notes;
+    }
 }
 ?>
--- tests/testClient.php
+++ tests/testClient.php
@@ -2,7 +2,7 @@
 include_once(dirname(__FILE__).'/../domain/Client.php');
 class testClient extends UnitTestCase {
     function testClientModule() {     
-                 
+    				
     }
 }
 
