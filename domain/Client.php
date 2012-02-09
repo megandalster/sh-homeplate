ed575bd376aa2ebedb2820044e43d2d2650c1c58
Hartley Brody <hartley.brody@gmail.com>
2012-02-09 13:49:49 -0500

    fleshed out Client & testClient, but haven't gotten unit tests to actually run yet

--- domain/Client.php
+++ domain/Client.php
@@ -17,5 +17,80 @@ class Client {
 							// e.g. [ÒMondayÓ, ÒWednesdayÓ]
 	private $feed_america;	// ÒyesÓ or ÒnoÓ
 	private $notes; 		// notes written by the team captain or coordinator
+	
+	//copied from volunteer class. question about use of "explode" with arrays
+	
+	function __construct($id, $name, $chain_name, $area, $type, $address, $city, $state, $zip, $geocoordinates,
+	                        $phone1, $phone2, $days, $feed_america, $notes){                
+        $this->id       	= $id;      
+        $this->name 		= $name;      
+        $this->chain_name 	= $chain_name;      
+        $this->area 		= $area;      
+        $this->type 		= $type;      
+        $this->address 		= $address;      
+        $this->city 		= $city;      
+        $this->state 		= $state;      
+        $this->zip 			= $zip;
+
+        
+        $this->geocoordinates = $geocoordinates;
+        $this->phone1 		= $phone1;
+        $this->phone2 		= $phone2;
+        
+        $this->days 		= $days;
+        $this->feed_america	= $feed_america;
+        $this->notes 		= $notes;
+        
+        
+    }
+    //getter functions
+    function get_id() {
+        return $this->id;
+    }
+    function get_name() {
+        return $this->name;
+    }
+    function get_chain_name() {
+        return $this->chain_name;
+    }
+    function get_area() {
+        return $this->area;
+    }
+    function get_type() {
+        return $this->type;
+    }
+    function get_address() {
+        return $this->address;
+    }
+    function get_city() {
+        return $this->city;
+    }
+    function get_state() {
+        return $this->state;
+    }
+    function get_zip() {
+        return $this->zip;
+    }
+    function get_geo() {
+        return $this->geocoordinates;
+    }
+    function get_phone1() {
+        return $this->phone1;
+    }
+    function get_phone2() {
+        return $this->phone2;
+    }
+    function get_days(){
+        return $this->days;
+    }
+    function is_feed_america(){
+        return $this->feed_america;
+    }
+    function get_notes(){
+        return $this->notes;
+    }
+    
+    //setter functions ... can be added later as needed
+        
 }
 ?>
