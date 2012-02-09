b0452fe4f401347b5dff82d4b88a7986830cef0b
Hartley Brody <hartley.brody@gmail.com>
2012-02-09 15:07:45 -0500

    got tests working

--- tests/testClient.php
+++ tests/testClient.php
@@ -4,13 +4,14 @@ class testClient extends UnitTestCase {
     function testClientModule() {  
 
     	//fake client to test
-        $volunteer = new Client("some-id-01", "Whole Foods", "Whole Foods USA", "Bowdoin", "restaurant", 
+        $client = new Client("some-id-01", "Whole Foods", "Whole Foods USA", "Bowdoin", "restaurant", 
         					"123 Maine St", "Brunswick", "ME", "04011", "000-000-000",
-	                        "(207)725-3500", "", "Monday", True, "This is a test case");
+	                        "(207)725-3500", "", "", True, "This is a test case");
         
         echo("got here 1");
         
         //testing getter functions
+        
         $this->assertTrue($client->get_name() == "Whole Foods");
         $this->assertTrue($client->get_chain_name() == "Whole Foods USA");
         $this->assertTrue($client->get_address() == "123 Maine St");
@@ -21,9 +22,10 @@ class testClient extends UnitTestCase {
         $this->assertTrue($client->get_phone2() == "");
         $this->assertTrue($client->get_notes() == "This is a test case");
         
-        $this->assertTrue(1 == 1);
+        //$this->assertTrue(1 == 1);
+                 
         //tests the 'is_type' function
-        $this->assertTrue($client->is_feed_america(True));
+        $this->assertTrue($client->is_feed_america() == True);
       
                  
         echo ("testVolunteer complete\n");
