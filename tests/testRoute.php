735be317088b093559841a2f97ff863e78f36c82
allen <allen@localhost>
2012-02-27 09:01:56 -0500

    more changes to Route, testRoute, and testStop

--- tests/testRoute.php
+++ tests/testRoute.php
@@ -2,13 +2,13 @@
 include_once(dirname(__FILE__).'/../domain/Route.php');
 class testRoute extends UnitTestCase {
     function testRouteModule() {    
-    	
     	//fake route to test
-    	$route = new Route("11-12-28-HHI", "1, 2, 3", "Team Captain","", "Note.");
+    	$route = new Route("11-12-28-HHI", "malcom1234567890,sandi8437891234", "Jon1112345678",
+    	"Food Lion - Palmetto Bay,Piggly Wiggly", "Note.");
     	
     	//testing getter functions
     	$this->assertTrue($route->get_id() == "11-12-28-HHI");
-    	$this->assertTrue($route->get_teamcaptain_id() == "Team Captain");
+    	$this->assertTrue($route->get_teamcaptain_id() == "Jon1112345678");
     	$this->assertEqual($route->get_day(), "Wednesday December 28, 2011");
 	   	$this->assertEqual($route->get_area(), "Hilton Head Island");
     	$this->assertTrue($route->get_notes() == "Note.");
