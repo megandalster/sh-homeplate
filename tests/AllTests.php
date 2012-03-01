a58075571d55389162b59a8591c576f06fbd340e
Richardo Hopkins <milkywayw@gmail.com>
2012-03-01 11:24:36 -0500

    Wrote the database module for the Route domain (dbRoutes) and implemented its test module (testdbRoutes)

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -4,15 +4,17 @@
  */
 // require_once(dirname(__FILE__).'/simpletest/autorun.php');
 class AllTests extends GroupTest {
- 	  function AllTests() {
+ 	  function AllTests() {/*
         $this->addTestFile(dirname(__FILE__).'/testVolunteer.php');
         $this->addTestFile(dirname(__FILE__).'/testClient.php');
-        $this->addTestFile(dirname(__FILE__).'/testRoute.php');
-        $this->addTestFile(dirname(__FILE__).'/testStop.php');
-        $this->addTestFile(dirname(__FILE__).'/testWeek.php');
-        $this->addTestFile(dirname(__FILE__).'/testScheduleEntry.php');
-        $this->addTestFile(dirname(__FILE__).'/testMonth.php');
-        $this->addTestFile(dirname(__FILE__).'/testdbVolunteers.php');
+       */ $this->addTestFile(dirname(__FILE__).'/testRoute.php');
+        //$this->addTestFile(dirname(__FILE__).'/testStop.php');
+        //$this->addTestFile(dirname(__FILE__).'/testWeek.php');
+        //$this->addTestFile(dirname(__FILE__).'/testScheduleEntry.php');
+
+        //$this->addTestFile(dirname(__FILE__).'/testMonth.php');	*/
+        $this->addTestFile(dirname(__FILE__).'/testdbRoutes.php');
+        //$this->addTestFile(dirname(__FILE__).'/testdbVolunteers.php');
         echo ("All tests complete");
  	  }
  }
