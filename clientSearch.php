10e4fcb22680fb42dad2fee92099173fc6411c6f
Hartley Brody <hartley.brody@gmail.com>
2012-04-19 01:34:41 -0400

    got client search working\!

--- clientSearch.php
+++ clientSearch.php
@@ -32,64 +32,74 @@
 						echo('<p><strong>Search for clients:</strong>');
 						echo '<p>Area: <select name="s_area">' .
 							'<option value=""></option>'; 
-						echo '<option value="HHI"'; if ($area=="HHI") echo " SELECTED"; echo '>Hilton Head</option>' ;
-						echo '<option value="SUN"'; if ($area=="SUN") echo " SELECTED"; echo '>Sun City</option>' ;
-						echo '<option value="BFT"'; if ($area=="BFT") echo " SELECTED"; echo '>Beaufort</option>';
+                            echo '<option value="HHI"'; if ($area=="HHI") echo " SELECTED"; echo '>Hilton Head</option>' ;
+                            echo '<option value="SUN"'; if ($area=="SUN") echo " SELECTED"; echo '>Sun City</option>' ;
+                            echo '<option value="BFT"'; if ($area=="BFT") echo " SELECTED"; echo '>Beaufort</option>';
 						echo '</select>';
-						echo('&nbsp;&nbsp;Type:<select name="s_type">' .
-                                '<option value="" SELECTED></option>' . 
-                                '<option value="donor">Donor</option>' . 
-                                '<option value="recipient">Recipient</option>' .
-							'</select>');
-						echo('&nbsp;&nbsp;Feed America:<select name="s_status">' .
-                                '<option value=""></option>' . 
-                                '<option value="yes">Yes</option>' .
-                                '<option value="no">No</option>' .
-							'</select>');
+                        
+                        if( !array_key_exists('s_type', $_POST) ) $type = ""; else $type = $_POST['s_type'];
+						echo '&nbsp;&nbsp;Type:<select name="s_type">';
+                            echo '<option value=""'; if ($type=="") echo " SELECTED"; echo '></option>'; 
+                            echo '<option value="donor"'; if ($type=="donor") echo " SELECTED"; echo '>Donor</option>'; 
+                            echo '<option value="recipient"'; if ($type=="recipient") echo " SELECTED"; echo '>Recipient</option>'; 
+                        echo '</select>';
+                        
+                        if( !array_key_exists('s_status', $_POST) ) $status = ""; else $status = $_POST['s_status'];
+						echo '&nbsp;&nbsp;Feed America:<select name="s_status">';
+                                echo '<option value=""'; if ($status=="") echo " SELECTED"; echo '></option>';
+                                echo '<option value="yes"'; if ($status=="yes") echo " SELECTED"; echo '>Yes</option>';
+                                echo '<option value="no"'; if ($status=="no") echo " SELECTED"; echo '>No</option>';
+                        echo '</select>';
 						echo '&nbsp;&nbsp;Name: ' ;
 						echo '<input type="text" name="s_name">';
 						echo '<fieldset>';
 						echo '<legend>Availability:</legend>';
-						echo '<p id="s_day"><label>Day:&nbsp;&nbsp;&nbsp;&nbsp;</label>';
+						echo '<p id="s_day">Day:&nbsp;&nbsp;&nbsp;&nbsp;';
 							foreach($days as $day=>$dayname)
-							  echo '<input type="checkbox" name="s_day[]" value='.$day.' />'.$day;
+							  echo '<label><input type="checkbox" name="s_day[]" value='.$day.' />'.$day.'</label>';
 						echo '</fieldset>';
 						echo('<p><input type="hidden" name="s_submitted" value="1"><input type="submit" name="Search" value="Search">');
 						echo('</form></p>');
 					
 				// if user hit "Search"  button, query the database and display the results
-					if($_POST['s_submitted']){
+					if( array_key_exists('s_submitted', $_POST) ){
 						$area = $_POST['s_area'];
 						$type = $_POST['s_type'];
 						$status = $_POST['s_status'];
                         $name = trim(str_replace('\'','&#39;',htmlentities($_POST['s_name'])));
+                        
                         $availability = array();
-       /*                 if (!$_POST['s_day'])      // allow "any" day if none checked
-                        	$_POST['s_day'][] = "";
-                        if (!$_POST['s_week'])	   // allow "any" week if none checked
-                        	$_POST['s_week'][] = "";
-       */                 foreach ($_POST['s_day'] as $day) 
+                        if ( !array_key_exists('s_day', $_POST) ) 
+                            $_POST['s_day'][] = ""; // allow "any" day if none checked
+                        foreach ($_POST['s_day'] as $day) 
                         		$availability[] = $day;
-                        // now go after the volunteers that fit the search criteria
-                        include_once('database/dbVolunteers.php');
-     					include_once('domain/Volunteer.php');
+                        
+                        //print_r( $availability );
      					//echo "search criteria: ", $area.$type.$status.$name.$availability[0];
-						$result = getonlythose_dbVolunteers($area, $type, $status, $name, $availability);
-
-						echo '<p><strong>Search Results:</strong> <p>Found '.sizeof($result).' '.$status.' '.
-							$type.'s';
+     					//echo "type: ", $type;
+                        
+                        // now go after the volunteers that fit the search criteria
+                        include_once('database/dbClients.php');
+     					include_once('domain/Client.php');
+						
+                        $result = getall_clients($area, $type, $status, $name, $availability);
+                        //print_r( $result );
+						
+                        echo '<p><strong>Search Results:</strong> <p>Found ' . sizeof($result). ' ';
+                            if (!$type) echo "client(s)"; 
+                            else echo $type.'s';
 						if ($areas[$area]!="") echo ' from '.$areas[$area];
 						if ($name!="") echo ' with name like "'.$name.'"';
 						if (sizeof($result)>0) {
 							echo ' (select one for more info).';
-							echo '<p><table> <tr><td>Name</td><td>Phone</td><td>E-mail</td><td>Monthly Availability</td></tr>';
-							foreach ($result as $vol) {
-								echo "<tr><td><a href=volunteerEdit.php?id=".$vol->get_id().">" . 
-									$vol->get_first_name() .  " " . $vol->get_last_name() . "</td><td>" . 
-									phone_edit($vol->get_phone1()) . "</td><td>" . 
-									$vol->get_email() . "</td><td>";
-								foreach($vol->get_availability() as $availableon)
-									echo ($weeks[substr($availableon,4,1)] . " " . $days[substr($availableon,0,3)] . ", ") ;
+							echo '<p><table> <tr><td><strong>Name</strong></td><td><strong>Phone</strong></td><td><strong>Area</strong></td><td><strong>Availability</strong></td></tr>';
+							foreach ($result as $client) {
+								echo ("<tr><td><a href='clientEdit.php?id=" . $client->get_id() ."'>" .
+									$client->get_id() . "</td><td>" . 
+									$client->get_phone1() . "</td><td>" . 
+									$client->get_area() . "</td><td>");
+								foreach($client->get_days() as $availableon)
+									echo ( $availableon . ", ") ;
 								echo "</td></a></tr>";
 							}
 						}
