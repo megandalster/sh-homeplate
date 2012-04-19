9e24edce9cd972af503f0104558e33fca5cb20c4
nwetzel41 <nwetzel41@localhost>
2012-04-18 21:35:47 -0400

    Created three different Stop views and modified the Client associated classes

--- tests/testClient.php
+++ tests/testClient.php
@@ -6,7 +6,7 @@ class testClient extends UnitTestCase {
     	//fake client to test
         $client = new Client("Whole Foods Brunswick", "Whole Foods USA", "Bowdoin", "restaurant", 
         					"123 Maine St", "Brunswick", "ME", "04011", "",
-	                        "2077253500", "", "Mon,Wed", True, "This is a test case");
+	                        "2077253500", "", "Mon,Wed", True, "pounds", "This is a test case");
         
          //testing getter functions
         
@@ -19,6 +19,7 @@ class testClient extends UnitTestCase {
         $this->assertTrue($client->get_phone1() == "2077253500");
         $this->assertTrue($client->get_phone2() == "");
         $this->assertTrue(sizeof($client->get_days()) == 2);
+        $this->assertTrue($client->get_weight_type() == "pounds");
         $this->assertTrue($client->get_notes() == "This is a test case");
         
         //$this->assertTrue(1 == 1);
--- viewStop1.php
+++ viewStop1.php
@@ -30,14 +30,31 @@
 	$client_type = "Donor";
 	$client_items = "";
 	
-	$total_weight = isset($_POST["total_weight"]) ? $_POST["total_weight"] : "0";
+	$meat_weight = isset($_POST["meat_weight"]) ? $_POST["meat_weight"] : "0";
+	$bakery_weight = isset($_POST["bakery_weight"]) ? $_POST["bakery_weight"] : "0";
+	$dairy_weight = isset($_POST["dairy_weight"]) ? $_POST["dairy_weight"] : "0";
+	$produce_weight = isset($_POST["produce_weight"]) ? $_POST["produce_weight"] : "0";
+	$grocery_weight = isset($_POST["grocery_weight"]) ? $_POST["grocery_weight"] : "0";
 	$driver_notes = isset($_POST["driver_notes"]) ? $_POST["driver_notes"] : "";
 	
-	// $stop1 = retrieve_dbStops($id);
+	// $stop1 = retrieve_dbStops($id); 
 	$stop1 = new Stop($ndate."-".$area, $client_id, $client_type, $client_items, $driver_notes);
 	insert_dbStops($stop1);
 		
-	$stop1->set_total_weight($total_weight);
+	$item1 = "Meat:" . $meat_weight;
+	$stop1->add_item($item1);
+				
+	$item2 = "Bakery:" . $bakery_weight;
+	$stop1->add_item($item2);
+				
+	$item3 = "Dairy:" . $dairy_weight;
+	$stop1->add_item($item3);
+		
+	$item4 = "Produce:" . $produce_weight;
+	$stop1->add_item($item4);
+		
+	$item5 = "Grocery:" . $grocery_weight;
+	$stop1->add_item($item5);
 		
 	$stop1->set_notes($driver_notes);
 		
@@ -63,24 +80,60 @@
 			   Date: <?php echo($date)?></p>
 			   
 			<p>Instructions:<br /><br />
-			   1. To insert the total weight, tap the corresponding text box.<br />
+			   1. To insert a value, tap the corresponding cell in the table below.<br />
 			   2. Insert additional notes into the text box below the table if necessary.<br />
 			   3. When the values are entered, tap the "Submit" button at the bottom.<br />
 			   4. After all values are submitted, tap the "Return to Route" button at the bottom.
 			</p>
 			
-			<form method = "post"?>
-				Total Weight: <input type = "text" name = "total_weight" value = <?php echo $total_weight ?> />
+			<form method="post"?>
+			<table border = "1" cellpadding = "5">
+				<tr>
+					<td><b>Type</b></td>
+					<td>Weight (lbs.)</td>
+					
+				</tr>
+				<tr>
+					<td><b>Meat</b></td>
+					<td><input type="text" name="meat_weight" value= <?php echo $meat_weight?> /></td>
+					
+				</tr>
+				<tr>
+					<td><b>Bakery</b></td>
+					<td><input type="text" name="bakery_weight" value= <?php echo $bakery_weight?> /></td>
+					
+				</tr>
+				<tr>
+					<td><b>Dairy</b></td>
+					<td><input type="text" name="dairy_weight" value= <?php echo $dairy_weight?> /></td>
+					
+				</tr>
+				<tr>
+					<td><b>Produce</b></td>
+					<td><input type="text" name="produce_weight" value= <?php echo $produce_weight?> /></td>
+					
+				</tr>
+				<tr>
+					<td><b>Grocery</b></td>
+					<td><input type="text" name="grocery_weight" value= <?php echo $grocery_weight?> /></td>
+					
+					
+				</tr>
+				<tr>
+					<td><b>Total</b></td>
+					<td><input type="text" name="total_weight" readonly="readonly" 
+								value=<?php echo($meat_weight + $bakery_weight + $dairy_weight + 
+								$produce_weight + $grocery_weight)?> /></td>
+				</tr>	
+			</table>	
 				<br />
-				
 			<p>Enter any additional notes by tapping the text box below:</p>
-			<textarea rows="10" cols="50" name="driver_notes"></textarea>
+			<textarea rows="10" cols="50" name="driver_notes" ></textarea>
 				<br />
 				<br />
-			<input type="submit" value="Submit"/>
+			<input type="submit" value="Submit" />
 			</form>
-				<br/>
-				<br/>
+				<br />
 			<button type="submit" onclick="">Return to Route</button>
 			
 			</div>
