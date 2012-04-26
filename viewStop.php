7b744eccd011e7192612a49fdec021a4618ef736
nwetzel41 <nwetzel41@localhost>
2012-04-26 12:00:33 -0400

    Updated the viewStops pages into one script and cleaned up each viewStop page. Started implementing the editRoute page but it still needs work

--- viewStop.php
+++ viewStop.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Copyright 2012 by Hartley Brody, Richardo Hopkins, Nicholas Wetzel, and Allen 
  * Tucker.  This program is part of Homeplate, which is free software.  It comes 
@@ -11,80 +10,33 @@
 /*
  * viewStop GUI for Homeplate
  * @author Nicholas Wetzel
- * @version April 4, 2012
+ * @version April 25, 2012
  */
 
 	session_start();
 	session_cache_expire(30);
 	
-	include_once('database/dbStops.php');
-	include_once('domain/Stop.php');
-	
+	include_once('database/dbClients.php');	
+	include_once('domain/Client.php');
+
 	//$area = $_GET["area"];
-	$area = "Hilton Head";
-	//$date = $_GET["date"];
-	$date = date('l, F j, Y');
-	//$id = $date."-".$area.".".$client_id;
-	$ndate = date('y-m-d');
-	$client_id = "Bi-Lo - HHI North";
-	$client_type = "Donor";
-	$client_items = "";
+	//$client_id = $_GET["client_id"];
 	
-	$total_weight = isset($_POST["total_weight"]) ? $_POST["total_weight"] : "0";
-	$driver_notes = isset($_POST["driver_notes"]) ? $_POST["driver_notes"] : "";
+	//$client1 = retrieve_dbClients($client_id);
+	//$client_type = $client1->get_weight_type();
+	$client_type = 'foodtype';
 	
-	// $stop1 = retrieve_dbStops($id);
-	$stop1 = new Stop($ndate."-".$area, $client_id, $client_type, $client_items, $driver_notes);
-	insert_dbStops($stop1);
-		
-	$stop1->set_total_weight($total_weight);
-		
-	$stop1->set_notes($driver_notes);
-		
-	update_dbStops($stop1);
-	
+	switch ($client_type)
+	{
+		case 'pounds':
+			include ('viewStop1.php');
+			break;
+			
+		case 'foodtype':
+			include ('viewStop2.php');
+			break;
+			
+		case 'foodtypeboxes':
+			include ('viewStop3.php');
+	}
 ?>
-<html>
-	<head>
-		<title>
-			Viewing <?php echo($area."-".$client_id."-".$date)?>;
-		</title>
-	</head>
-	<body>
-		<div id="container">
-			<?php include('header.php');?>
-			<div id="content">
-  
-			<p><big><?php echo($client_id)?></big></p>
-			
-			<p>Route: <?php echo($area)?><br />
-			   Driver: <?php // echo($volunteer->get_first_name()." ".$volunteer->get_last_name())?>
-			   			Nick Wetzel<br />
-			   Date: <?php echo($date)?></p>
-			   
-			<p>Instructions:<br /><br />
-			   1. To insert the total weight, tap the corresponding text box.<br />
-			   2. Insert additional notes into the text box below the table if necessary.<br />
-			   3. When the values are entered, tap the "Submit" button at the bottom.<br />
-			   4. After all values are submitted, tap the "Return to Route" button at the bottom.
-			</p>
-			
-			<form method = "post"?>
-				Total Weight: <input type = "text" name = "total_weight" value = <?php echo $total_weight ?> />
-				<br />
-				
-			<p>Enter any additional notes by tapping the text box below:</p>
-			<textarea rows="10" cols="50" name="driver_notes"></textarea>
-				<br />
-				<br />
-			<input type="submit" value="Submit"/>
-			</form>
-				<br/>
-				<br/>
-			<button type="submit" onclick="">Return to Route</button>
-			
-			</div>
-			<?PHP include('footer.inc');?>
-		</div>
-	</body>
-</html>
--- viewStop3.php
+++ viewStop3.php
@@ -13,7 +13,7 @@
  * @author Nicholas Wetzel
  * @version April 4, 2012
  */
-
+	
 	include_once('database/dbStops.php');
 	include_once('domain/Stop.php');
 	
@@ -27,14 +27,45 @@
 	$client_type = "Donor";
 	$client_items = "";
 	
-	$total_weight = isset($_POST["total_weight"]) ? $_POST["total_weight"] : "0";
+	$meat_percent = 0.25;
+	$dairy_percent = 0.03;
+	$bakery_percent = 0.31;
+	$produce_percent = 0.18;
+	$dry_goods_percent = 0.23;
+	$pounds_per_carton = 32;
+	
+	/*if($_GET["area"]=="SUN"){
+		$pounds_per_carton = 32;
+	}
+	if($_GET["area"]=="BFT"){
+		$pounds_per_carton = 44;
+	}
+	*/
+	
+	$total_cartons = isset($_POST["total_cartons"]) ? $_POST["total_cartons"] : "0";
 	$driver_notes = isset($_POST["driver_notes"]) ? $_POST["driver_notes"] : "";
+	
 	
 	//$stop1 = retrieve_dbStops($id);
 	$stop1 = new Stop($ndate."-".$area, $client_id, $client_type, $client_items, $driver_notes);
 	insert_dbStops($stop1);
 		
-	$stop1->set_total_weight($total_weight);
+	$total_weight = $total_cartons * $pounds_per_carton;
+		
+	$item1 = "Meat:" . $total_weight * $meat_percent;
+	$stop1->add_item($item1);
+			
+	$item2 = "Bakery:" . $total_weight * $bakery_percent;
+	$stop1->add_item($item2);
+				
+	$item3 = "Dairy:" . $total_weight * $dairy_percent;
+	$stop1->add_item($item3);
+		
+	$item4 = "Produce:" . $total_weight * $produce_percent;
+	$stop1->add_item($item4);
+		
+	$item5 = "Dry Goods:" . $total_weight * $dry_goods_percent;
+	$stop1->add_item($item5);
 		
 	$stop1->set_notes($driver_notes);
 		
@@ -60,49 +91,49 @@
 			   			Nick Wetzel<br />
 			   Date: <?php echo($date)?></p>
 			   
-			<fieldset> 
+			<fieldset>
 				<legend><b>Instructions:</b></legend><br />
-			   <b>1.</b> To insert the total weight, tap the corresponding text box.<br /><br />
-			   <b>2.</b> Insert additional notes into the text box below the table if necessary.<br /><br />
+			   <b>1.</b> To insert the total number of cartons, tap the corresponding text box below.<br /><br />
+			   <b>2.</b> Insert additional notes into the text box below if necessary.<br /><br />
 			   <b>3.</b> When the values are entered, tap the "Submit" button at the bottom.<br /><br />
-			   <b>4.</b> Check that the submitted values in the red box at the bottom of the screen are correct. If not, re-enter them.<br /><br />
-			   <b>5.</b> After all values are correct, tap the "Return to Route" button at the bottom.<br />
+			   <b>4.</b> Check that the submitted values in the red box at the bottom of the screen are correct. If not, re-enter them.<br /><br /> 
+			   <b>5.</b> After all values are submitted, tap the "Return to Route" button at the bottom.<br />
 			</fieldset>
 			
-			<form method = "post">
+			<form method="post"?>
 			<fieldset>
 				<legend><b>Data Entry:</b></legend><br />
-				<b>Total Weight:</b> <input type = "text" name = "total_weight" value = <?php echo $total_weight?> /> lbs.
+				<b>Total Cartons:</b> <input type = "text" name = "total_cartons" value = <?php echo $total_cartons?> />
 				<br />
-				
 			<p>Enter any additional notes by tapping the text box below:</p>
-			<textarea rows="10" cols="50" name="driver_notes"></textarea>
-				
-			<input type = "hidden" name = "submitted" value = "true"/>	
 			
+			<textarea rows="10" cols="50" name="driver_notes" ></textarea>
+				
+			<input type = "hidden" name = "submitted" value = "true"/>
+				
 				<br />
 				<br />
 				
-			<input type="submit" value="Submit"/>
+			<input type="submit" value="Submit" />
 			</fieldset>
 			</form><br />
-			
+				
 			<?php 
 			if (isset($_POST['submitted'])){
 				echo('
 				<div class = "warning">
 					<b>Submitted Values:</b><br/><br/>
-					Total Weight: <b>'.$total_weight.'</b> lbs.<br/><br/>
+					Total Cartons: <b>'.$total_cartons.'</b><br/><br/>
 					Notes:'.$driver_notes.'
 				</div>
 				');
 			}
 			?>
 			
-			<br/>
-			<br/>
-			<br/>
-				
+			<br />
+			<br />
+			<br />
+			
 			<a href="<?php echo $path?>editRoute.php"><big>Return to Route</big></a>
 			
 			</div>
