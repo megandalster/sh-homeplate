e4230184346a14c1a0516a72ede04ebe3a0fd843
Richardo Hopkins <milkywayw@gmail.com>
2012-05-01 18:57:44 -0400

    Fully implemented the viewReports page to display reports for a given week, month, or date range. Also included a MINOR change to getall_stops_for_date_range in dbStops to correctly display a stop's weight. Still need to implement previous and next week options, and to add save/export functionality in the future.

--- viewReports.php
+++ viewReports.php
@@ -3,9 +3,9 @@
 /*
  *  Our copyright notice
  */
- 
-	session_start();
-	session_cache_expire(30)
+
+session_start();
+session_cache_expire(30)
 ?>
 <html>
 <head>
@@ -15,12 +15,12 @@
 <body>
 <div id="container"><?php include('header.php');?>
 <div id="content"><?php 
-					include_once('database/dbStops.php');
-					include_once('domain/Stop.php');
-					
-					$time = time();
-					echo "<h4>Today is ".date('l F j, Y', $time)."</h4>";
-				?>
+include_once('database/dbStops.php');
+include_once('domain/Stop.php');
+
+$time = time();
+echo "<h4>Today is ".date('l F j, Y', $time)."</h4>";
+?>
 
 <form method="post" action="">
 <p>Area : <select name="report_area">
@@ -43,7 +43,7 @@
 <input type="radio" name="report_span" value="monthly" />
 Monthly&nbsp;&nbsp; <?php 
 
-$months = array ("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+$months = array ("Jan", "Feb", "Mar", "Apr", "May", "Jun",
 				 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 
 // month selector
@@ -64,8 +64,6 @@ for($i = $current_time[year]; $i >= 1990; $i--)
 }
 echo '</select>';
 ?>
-
-
 <br>
 <input type="radio" name="report_span" value="range" /> Date
 Range&nbsp;&nbsp; <?php 
@@ -122,124 +120,159 @@ for($i = $current_time[year]; $i >= 1990; $i--)
 	echo '<option value="'.$i.'">'.$i.'</option>';
 }
 echo '</select>';
-?>
-</fieldset>
+?></fieldset>
 </p>
 
-<!-- submit button -->
-<br>
+<!-- submit button --> <br>
 <input type="hidden" name="submitted" value="1"><input
-	type="submit" name="Generate " value="Generate Report">
+	type="submit" name="Generate " value="Generate Report"></form>
 
-</form>
-
-<?php 
+<?php
 if($_POST['submitted'])
 {
 	echo "<hr />";
-		
+
 	if($_POST['report_span'] == "weekly")
 	{
 		$time = strtotime('monday this week');
-		
-		
+		$endTime = strtotime("+1 week", $time);
+
+		$start_date = date('y-m-d', $time);
+		$end_date = date('y-m-d', $endTime);
+
 		echo "<br><p>";
 		echo "<b><font color='orange'>Week of ".date('l F j, Y', $time). "</b></font>";
 		echo "</p><br>";
-		
-		echo '<table cellspacing="10">';
-		echo '<style type="text/css">';
-		echo "td {padding: 10px 30px 10px 30px;}";
-		echo "</style>";
-		
-		echo "<tr>";
-		//echo "<td><b><font color='orange'>Week of:</b><br><u>". date('m/d/Y', $time) ."</u></font></td>";
-		echo "<th></th>";
-		echo "<td><b> Donors </b></td>";
-		echo "<td><b> Recipients </b></td>";
-		echo "</tr>";
-	
-		$weekday = array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
-	
-		// each iteration generates a row in the table
-		foreach($weekday as $value)
-		{			
-			// start row
-			echo "<tr>" ;
-			
-			// col 1 : day of week
-			echo "<td><b>".$value."</b><br>";
-			echo '<font size="1">' .date("F jS, Y", strtotime($value." this week")). "</font></td>" ;
-
-			// col 2 : status (blank)
-			echo "<td>not yet created</td>";
-				
-			// col 3 : view/edit
-			echo "<td>null</td>";
-
-			// end row
-			echo "</tr>";
-		}
-		
-		echo "</table>";
 	}
-	
+
 	if($_POST['report_span'] == "monthly")
 	{
 		$month = $_POST['monthly_month'];
 		$year = $_POST['monthly_year'];
-		
+
 		$time = strtotime("first day of ".$month.' '.$year);
-		
+		$endTime = strtotime("last day of ".$month.' '.$year);
+
+		$start_date = date('y-m-d', $time);
+		$end_date = date('y-m-d', $endTime);
+
 		echo "<br><p>";
-		echo "<b><font color='orange'>".date('F Y', $time). "</b></font>";
+		echo "<b><font color='orange'>".date(' F Y', $time). "</b></font>";
 		echo "</p><br>";
-		
-		echo '<table cellspacing="10">';
-		echo '<style type="text/css">';
-		echo "td {padding: 10px 30px 10px 30px;}";
-		echo "</style>";
-		
-		echo "<tr>";
-		echo "<th></th>";
-		echo "<td><b> Donors </b></td>";
-		echo "<td><b> Recipients </b></td>";
-		echo "</tr>";
-	
-		$maxtime = strtotime("+1 month", $time);
-		
-		// each iteration generates a row in the table
-		for($time; $time < $maxtime; $time = strtotime("+1 day", $time))
-		{			
-			// start row
-			if(date("D", $time) == "Sun")
-				echo '<tr class="border-bottom">';
-			else
-				echo "<tr>" ;
-			
-			// col 1 : day of week
-			echo "<td><b>".date("l",$time)."</b><br>";
-			echo '<font size="1">' .date("F jS, Y", $time). "</font></td>" ;
-
-			// col 2 : status (blank)
-			echo "<td>none</td>";
-				
-			// col 3 : view/edit
-			echo "<td>none</td>";
-				
-			// end row
-			echo "</tr>";
-			
-		}
-		
-		echo "</table>";
 	}
+
+	if($_POST['report_span'] == "range")
+	{
+		$time = strtotime($_POST['range_day_start'].$_POST['range_month_start'].$_POST['range_year_start']);
+		$endTime = strtotime($_POST['range_day_end'].$_POST['range_month_end'].$_POST['range_year_end']);
+
+		$start_date = date('y-m-d', $time);
+		$end_date = date('y-m-d', $endTime);
+
+		echo "<br><p>";
+
+		if($endTime >= $time)
+		{
+			echo "<b><font color='orange'>".date('F j, Y', $time);
+			echo "&nbsp;&nbsp;->&nbsp;&nbsp;".date('F j, Y', $endTime)."</b></font>";
+		}
+		else
+		{
+			echo "<b><font color='red'>Invalid date range! </b></font>";
+			echo "Starting date should be before or equal to end date";
+		}
+
+		echo "</p><br>";
+	}
+
+	// get list of all stops from database for date range
+	$all_stops = getall_dbStops_between_dates($start_date, $end_date);
+
+	//split all_stops into 2 different arrays - one for each
+	$pickups = array();
+	$dropoffs = array();
+
+	// assign each stop by type to corresponding array
+	foreach($all_stops as $stop)
+	{
+		if($stop->get_type() == "pickup")
+		$pickups[] = $stop;
+		else
+		$dropoffs[] = $stop;
+	}
+
+	// keep track of total weights
+	$tw_pickups  = 0;
+	$tw_dropoffs = 0;
+
+	echo '<table cellspacing="10">';
+	echo '<style type="text/css">';
+	echo "td {padding: 10px 30px 10px 30px;}";
+	echo "</style>";
+
+	echo "<tr>";
+
+	echo "<th>Donor</th>";
+	echo "<th>Weight</th>";
+	echo "<th>Recipient</th>";
+	echo "<th>Weight</th>";
+
+	echo "</tr>";
+
+	// iterator control
+	$max = (count($pickups) > count($dropoffs)) ? count($pickups) : count($dropoffs);
+
+	// each iteration generates a row in the table
+	for($i = 0; $i < $max; ++$i)
+	{
+		//echo '<font size="1">' .date("F jS, Y", strtotime($value." this week")). "</font></td>" ;
+
+		// start row
+		echo "<tr>" ;
+			
+		// cols 1 & 2 : donor name and weight
+		if($pickups[$i] != null)
+		{
+			echo "<td>".$pickups[$i]->get_client_id()."</td>";
+			echo "<td>".$pickups[$i]->get_total_weight()."</td>";
+			$tw_pickups += $pickups[$i]->get_total_weight();
+		}
+		else
+		{
+			echo "<td></td>";
+			echo "<td></td>";
+		}
+
+		// cols 3 & 4 : recipient name and weight
+		if($dropoffs[$i] != null)
+		{
+			echo "<td>".$dropoffs[$i]->get_client_id()."</td>";
+			echo "<td>".$dropoffs[$i]->get_total_weight()."</td>";
+			$tw_dropoffs += $dropoffs[$i]->get_total_weight();
+		}
+		else
+		{
+			echo "<td></td>";
+			echo "<td></td>";
+		}
+	}
+
+	// total weight row
+	echo "<tr>";
+
+	echo "<td><b>Total (Donors)</b></td>";
+	echo "<td>".$tw_pickups."</td>";
+	echo "<td><b>Total (Recipients)</b></td>";
+	echo "<td>".$tw_dropoffs."</td>";
+
+	echo "</tr>";
+
+	echo "</table>";
 }
+
 ?>
-
-
-	<?php include('footer.inc');?>
-</table>
+<?php include('footer.inc');?>
+<table></table>
 </div>
 </div>
 </body>
