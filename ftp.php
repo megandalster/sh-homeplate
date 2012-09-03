aafe84efae9140ffc88323a2b97d4bd89ecb0e27
allen <allen@bowdoin.edu>
2012-09-03 12:12:50 -0400

    added functions to handle tablets from multiple routes on same day and area

--- ftp.php
+++ ftp.php
@@ -10,22 +10,22 @@ function update_ftp() {
 	$areas = array("HHI"=>"Hilton Head", "SUN"=>"Bluffton", "BFT"=>"Beaufort");
 	autogenerate_routes();  // be sure we have next 3 weeks worth of routes in database
 	$todayUTC = time();
-	$mondaythisweek = strtotime('last monday', strtotime('tomorrow',$todayUTC));
+	$mondaylastweek = strtotime('last monday', $todayUTC);
 	$weekfromtodayUTC = $todayUTC+604800;
 	$mondaynextweek = strtotime('last monday', strtotime('tomorrow',$weekfromtodayUTC));
 	// we are mid-week and we want to update any new files that are there
-	for ($day = $mondaythisweek; $day < $mondaynextweek+604800; $day += 86400) {
+	for ($day = $mondaylastweek; $day < $mondaynextweek+604800; $day += 86400) {
 		if ($day > $todayUTC) 
 	  		ftpout($day, $areas); // update ftpout for future days
-	  	if ($day <= $todayUTC) 
-	  		ftpin($day);  // grab data from any past days, including today
+	  	if ($day < $todayUTC) 
+	  		ftpin($day);  // grab data from any past days, but not today!
 	}
 }
 
 function ftpout($day, $areas) {
 	$yymmdd = date('y-m-d',$day);
 	$fulldate = date('l F j, Y',$day);
-	$twoweeksagoyymmdd = date ('y-m-d',$day-1209600);
+	$twoweeksagoyymmdd = date('y-m-d',$day-1209600);
 	foreach ($areas as $area=>$area_name) {
 	// remove the file for 2 weeks ago from $day: date('y-m-d',$day-604800), if it's there
 	    $filename = dirname(__FILE__).'/../homeplateftp1/ftpout/'.$twoweeksagoyymmdd."-".$area.".csv";
@@ -82,27 +82,39 @@ function ftpout($day, $areas) {
 
 function ftpin($day) {
 	$areas = array("HHI"=>"Hilton Head", "SUN"=>"Bluffton", "BFT"=>"Beaufort");
-	$deviceIds = array("8c5328005a8d7784","6b2b51166c2b321f","387a6442e578d02f","bd3eb9c3c3bd44ba","c930db8fe6dccd30","3d28c762d6862027");
+	$deviceIds = array("8c5328005a8d7784", // allens tablet
+				"6b2b51166c2b321f","387a6442e578d02f","bd3eb9c3c3bd44ba",  //jons 6 tablets
+				"c930db8fe6dccd30","3d28c762d6862027","2fc8453a13e544a8");
 	$yymmdd = date('y-m-d',$day);
+	$twoweeksagoyymmdd = date('y-m-d',$day-1209600);
 	$day_of_week = date ("D", $day);
 	foreach ($areas as $area=>$area_name) {
 		foreach ($deviceIds as $deviceId) {
+	// get rid of old data
+	//		$filename = dirname(__FILE__).'/../homeplateftp1/ftpout/'.$twoweeksagoyymmdd."-".$area.".csv";
+	//		@unlink($filename);
 	// look for a file for $day and $deviceId
 			$filename = dirname(__FILE__).'/../homeplateftp1/ftpin/'.$yymmdd."-".$area."-".$deviceId.".csv";	
 			if (file_exists($filename)) {
 				$handle = fopen($filename, "r+");
 	// line 1			
-				$line1 = fgetcsv($handle, 0, ";");
-			//	echo "line 1 = ".$line1[0].$line1[1];
+				$line1 = fgetcsv($handle, 0, ";"); 
 				$id = substr($line1[0],0,12);
-				$notes = substr($line1[0],13).";".$line1[5]."-".$line1[6];
+				$tabletid = substr($line1[0],13);
+				$notes = $tabletid.";".$line1[5]."-".$line1[6];
+				$r = get_route($id);
+				if (strpos($r->get_notes(),$tabletid) > 0) { // WEIGHTS ALREADY RECORDED FOR THIS TABLET, SKIP IT
+					fclose($handle);
+					continue;
+				}
 				$teamcaptain = $line1[3];
+				
 	// line 2			
 				$drivers = array();
 				$availables = getall_drivers_available($area, $day_of_week);
 			//	echo "availables "; var_dump($availables);
-				$ds = fgetcsv($handle, 0, ";");
-				foreach ($ds as $d) {
+				$line2 = fgetcsv($handle, 0, ";");
+				foreach ($line2 as $d) {
 					if (strpos($d,"*")>0)
 					    continue;
 					$j = strpos($d,"#");
@@ -124,34 +136,43 @@ function ftpin($day) {
 			//	echo "line 2 = ".$drivers[0].$drivers[1];
 				
 	// line 3
-				$ps = fgetcsv($handle, 0, ";");
+				$line3 = fgetcsv($handle, 0, ";");
 				$pickup_stops = array();
-				foreach ($ps as $pickup_stop) {
+				foreach ($line3 as $pickup_stop) {
 					$pickup_stop = trim(str_replace('\'','',htmlentities(str_replace('\&','and',str_replace('\#',' ',$pickup_stop)))));
 					$pickup_stops[] = $id.$pickup_stop;
 				}
 			//	echo "line 3 = ".$pickup_stops[0].$pickup_stops[1];
 				
 	// line 4
-				$ds = fgetcsv($handle, 0, ";");
+				$line4 = fgetcsv($handle, 0, ";");
 				$dropoff_stops = array();
-				foreach ($ds as $dropoff_stop) {
+				foreach ($line4 as $dropoff_stop) {
 					$dropoff_stop = trim(str_replace('\'','',htmlentities(str_replace('\&','and',str_replace('\#',' ',$dropoff_stop)))));
 					$dropoff_stops[] = $id.$dropoff_stop;
 				}
 			//	echo "line 4 = ".$dropoff_stops[0].$dropoff_stops[1];
 				
-	// save the stuff
-				$r = get_route($id);
-				$r->set_drivers($drivers);
-				$r->set_pickup_stops($pickup_stops);
-				$r->set_dropoff_stops($dropoff_stops);
-				$r->set_notes($notes);
-				$r->set_status("completed");
+	// save and merge the data, allowing that more than one tablet has uploaded data 
+	// on the same day and area.
+	// do not allow more than one to pickup or dropoff at the same stop.
+				if ($r->get_status()=="completed") { // merging additional tablet's data
+					$r->merge_drivers($drivers);
+					$r->merge_pickup_stops(rebuild_original_stops($r, "pickup"), $pickup_stops);
+					$r->merge_dropoff_stops(rebuild_original_stops($r, "dropoff"), $dropoff_stops);
+					$r->merge_notes($notes);
+				}
+				else {
+					$r->set_drivers($drivers);
+					$r->set_pickup_stops($pickup_stops);
+					$r->set_dropoff_stops($dropoff_stops);
+					$r->set_notes($notes);
+					$r->set_status("completed");
+				}
 				if (has_nonzero_pickup_weight($r) || has_nonzero_dropoff_weight($r))
 					update_completed_dbRoutes($r);
 				// @unlink($filename);  // delete the file after saving its weights
-				// close the file
+				// rewrite the file and close it
 				fclose($handle);
 			}
 		}
